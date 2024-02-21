0e42ce8f8b9f16a902dc4230c3ea928cbaa6ced2
yukiSasaki <noitamina@fg8.so-net.ne.jp>
2024-02-22 00:43:55 +0900

    Start End Webサーバを立ち上げてWebの仕組みを知る（フォーム編）

--- webrick.rb
+++ webrick.rb
@@ -11,9 +11,27 @@ trap(:INT){
     server.shutdown
 }
 
-server.mount_proc("/time") do |req, res|
-  # レスポンス内容を出力
-  body ="<html><body>#{DateTime.now}</body></html>"
+server.mount_proc("/form_get") do |req, res|
+  #リクエストからデータを取得
+  form_data = req.query
+
+  #フォームから取得したデータの「name属性」をキーにして
+  body = 
+  "<html><body><head><meta charset='utf-8'></head><p>クエリパラメータは#{form_data }です</p><p>こんにちは#{form_data ["user_name"]}さん。あなたの年齢は#{form_data ["age"]}ですね</p></body></html>"
+
+  res.status = 200
+  res['Content-Type'] = 'text/html'
+  res.body = body
+end
+
+server.mount_proc("/form_post") do |req, res|
+  #リクエストからデータを取得
+  form_data = req.query
+
+  #フォームから取得したデータの「name属性」をキーにして
+  body = 
+  "<html><body><head><meta charset='utf-8'></head><p>クエリパラメータは#{form_data }です</p><p>こんにちは#{form_data ["user_name"]}さん。あなたの年齢は#{form_data ["age"]}ですね</p></body></html>"
+
   res.status = 200
   res['Content-Type'] = 'text/html'
   res.body = body
